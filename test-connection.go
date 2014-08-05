5c66a17727c9af471db77f0eed1267af85ac3df0
Elliot Shepherd <elliot@ninjablocks.com>
2014-08-05 11:29:26 +1000

    More testing, connect to all three servers

--- test-connection.go
+++ test-connection.go
@@ -7,21 +7,76 @@ import (
 	"log"
 	"net"
 	"os"
+	"os/signal"
 
 	"code.google.com/p/gogoprotobuf/proto"
 
 	"github.com/ninjasphere/go-zigbee/nwkmgr"
+
+	"github.com/ninjasphere/go-zigbee/gateway"
+	"github.com/ninjasphere/go-zigbee/otasrvr"
 )
 
 const (
-	hostname    = "beaglebone.local"
 	nwkmgrPort  = 2536
 	gatewayPort = 2541
-	otaPort     = 2540
+	otasrvrPort = 2540
 )
+
+// ZStackServer holds the connection to one of the Z-Stack servers (nwkmgr, gateway and otasrvr)
+type ZStackServer struct {
+	name      string
+	subsystem int8
+	conn      *net.TCPConn
+}
+
+func connectToServer(name string, subsystem int8, port int) (*ZStackServer, error) {
+
+	conn, err := net.DialTCP("tcp4", nil, &net.TCPAddr{
+		IP:   net.ParseIP("10.0.1.164"),
+		Port: port,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		for {
+			buf := make([]byte, 1024)
+			n, err := conn.Read(buf)
+			if err != nil {
+				log.Fatalf("whoops %s", err)
+			}
+			fmt.Printf("Received from %s (len:%d) : % X", name, n, buf[:n])
+		}
+	}()
+
+	return &ZStackServer{
+		name:      name,
+		subsystem: subsystem,
+		conn:      conn,
+	}, nil
+}
 
 func main() {
 	log.Println("Starting")
+
+	_, err := connectToServer("nwkmgr", int8(nwkmgr.ZStackNwkMgrSysIdT_RPC_SYS_PB_NWK_MGR), nwkmgrPort)
+	if err != nil {
+		// handle error
+		log.Printf("Error connecting nwkmgr %s", err)
+	}
+	_, err = connectToServer("gateway", int8(gateway.ZStackGwSysIdT_RPC_SYS_PB_GW), gatewayPort)
+	if err != nil {
+		// handle error
+		log.Printf("Error connecting gateway %s", err)
+	}
+	_, err = connectToServer("otasrvr", int8(otasrvr.ZStackOTASysIDs_RPC_SYS_PB_OTA_MGR), otasrvrPort)
+	if err != nil {
+		// handle error
+		log.Printf("Error connecting otasrvr %s", err)
+	}
 
 	/*joinTime := uint32(30)
 	command := &nwkmgr.NwkSetPermitJoinReq{
@@ -29,25 +84,20 @@ func main() {
 		PermitJoin:     nwkmgr.NwkPermitJoinTypeT_PERMIT_ALL.Enum(),
 	}*/
 
-	command := &nwkmgr.NwkZigbeeNwkInfoReq{}
+	//command := &nwkmgr.NwkZigbeeNwkInfoReq{}
+	//command := &nwkmgr.NwkGetLocalDeviceInfoReq{}
 
-	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", hostname, nwkmgrPort))
+	//time.Sleep(2000 * time.Millisecond)
 
-	sendNwkMgrCommand(command, command.GetCmdId(), conn)
+	//nwkmgrConn.sendCommand(command, int8(command.GetCmdId()))
 
-	reply := make([]byte, 1)
-
-	_, err = conn.Read(reply)
-	if err != nil {
-		println("Read from server failed:", err.Error())
-		os.Exit(1)
-	}
-
-	println("reply from server=", string(reply))
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+	fmt.Println("Got signal:", <-c)
 
 }
 
-func sendNwkMgrCommand(command proto.Message, commandID nwkmgr.NwkMgrCmdIdT, conn net.Conn) {
+func (s *ZStackServer) sendCommand(command proto.Message, commandID int8) {
 	proto.SetDefaults(command)
 
 	packet, err := proto.Marshal(command)
@@ -69,10 +119,9 @@ func sendNwkMgrCommand(command proto.Message, commandID nwkmgr.NwkMgrCmdIdT, con
 	err = binary.Write(buffer, binary.LittleEndian, int8(commandID))                                    // Command Id
 	_, err = buffer.Write(packet)
 
-	log.Printf("Created packet %x", buffer.Bytes())
+	log.Printf("%s: Sending packet: % X", s.name, buffer.Bytes())
 	//fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
 	//status, err := bufio.NewReader(conn).ReadString('\n')
 
-	_, err = conn.Write(buffer.Bytes())
-
+	//_, err = s.conn.Write(buffer.Bytes())
 }
