f23f46d63bf956c6afcbb1916c274cb2f3a5d834
Elliot Shepherd <elliot@ninjablocks.com>
2014-08-06 12:50:25 +1000

    Simple request/response handling

--- test-connection.go
+++ test-connection.go
@@ -3,12 +3,12 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
-	"time"
 
 	"code.google.com/p/gogoprotobuf/proto"
 
@@ -19,17 +19,186 @@ import (
 )
 
 const (
-  hostname = "10.0.1.164"
-  otasrvrPort  = 2525
-  gatewayPort = 2541
-  nwkmgrPort = 2540
+	hostname    = "beaglebone.local"
+	otasrvrPort = 2525
+	gatewayPort = 2541
+	nwkmgrPort  = 2540
 )
 
 // ZStackServer holds the connection to one of the Z-Stack servers (nwkmgr, gateway and otasrvr)
 type ZStackServer struct {
-	name      string
-	subsystem int8
-	conn      net.Conn
+	Incoming chan *[]byte // Incoming raw protobuf packets
+
+	name               string
+	subsystem          int8
+	conn               net.Conn
+	outgoing           chan *ZStackCommand // Outgoing protobuf messages
+	pendingByCommandID map[int8]*ZStackPendingResponse
+}
+
+func (s *ZStackServer) sendCommand(message *ZStackCommand) {
+	s.outgoing <- message
+}
+
+func (s *ZStackServer) sendRequest(request *ZStackCommand, response *ZStackCommand) (chan error, error) {
+	s.outgoing <- request
+
+	pending := &ZStackPendingResponse{
+		message:  response.message,
+		complete: make(chan error),
+	}
+
+	if s.pendingByCommandID[response.commandID] != nil {
+		return nil, fmt.Errorf("There is already a pending command waiting for the same response (Command ID: %X)", response.commandID)
+	}
+
+	s.pendingByCommandID[response.commandID] = pending
+
+	return pending.complete, nil
+}
+
+// ZStackCommand contains a protobuf message and a command id
+type ZStackCommand struct {
+	message   proto.Message
+	commandID int8
+}
+
+// ZStackPendingResponse contains the protobuf command to be filled with the response, and a 'complete' channel to indicate when done
+type ZStackPendingResponse struct {
+	message  proto.Message
+	complete chan error
+}
+
+type ZStackNwkMgrServer struct {
+	*ZStackServer
+}
+
+// SendCommand sends a protobuf Message to the Z-Stack server
+func (s *ZStackNwkMgrServer) SendCommand(command zStackNwkCommand) {
+	s.sendCommand(&ZStackCommand{
+		message:   command,
+		commandID: int8(command.GetCmdId()),
+	})
+}
+
+// SendRequest sends a protobuf Message to the Z-Stack server, and waits for the response
+func (s *ZStackNwkMgrServer) SendRequest(request zStackNwkCommand, response zStackNwkCommand) error {
+
+	complete, err := s.sendRequest(&ZStackCommand{
+		message:   request,
+		commandID: int8(request.GetCmdId()),
+	}, &ZStackCommand{
+		message:   response,
+		commandID: int8(response.GetCmdId()),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return <-complete
+}
+
+type zStackNwkCommand interface {
+	proto.Message
+	GetCmdId() nwkmgr.NwkMgrCmdIdT
+}
+
+type zStackGatewayMessage interface {
+	GetCmdId() gateway.GwCmdIdT
+}
+
+type zStackOtaMgrMessage interface {
+	GetCmdId() otasrvr.OtaMgrCmdIdT
+}
+
+func (s *ZStackServer) outgoingLoop() {
+	for {
+		command := <-s.outgoing
+
+		proto.SetDefaults(command.message)
+
+		packet, err := proto.Marshal(command.message)
+		if err != nil {
+			log.Fatal("marshaling error: ", err)
+		}
+
+		log.Printf("Protobuf packet %x", packet)
+
+		buffer := new(bytes.Buffer)
+
+		if err != nil {
+			// handle error
+			log.Printf("Error connecting %s", err)
+		}
+
+		// Add the Z-Stack 4-byte header
+		err = binary.Write(buffer, binary.LittleEndian, uint16(len(packet))) // Packet length
+		err = binary.Write(buffer, binary.LittleEndian, s.subsystem)         // Subsystem
+		err = binary.Write(buffer, binary.LittleEndian, command.commandID)   // Command Id
+
+		_, err = buffer.Write(packet)
+
+		log.Printf("%s: Sending packet: % X", s.name, buffer.Bytes())
+
+		// Send it to the Z-Stack server
+		_, err = s.conn.Write(buffer.Bytes())
+	}
+}
+
+func (s *ZStackServer) incomingLoop() {
+	for {
+		buf := make([]byte, 1024)
+		n, err := s.conn.Read(buf)
+		//log.Printf("Read %d from %s", n, s.name)
+		if err != nil {
+			log.Fatalf("%s: Error reading socket %s", s.name, err)
+		}
+		pos := 0
+
+		for {
+			var length uint16
+			var incomingSubsystem uint8
+			reader := bytes.NewReader(buf[pos:])
+			err := binary.Read(reader, binary.LittleEndian, &length)
+			if err != nil {
+				log.Fatalf("%s: Failed to read packet length %s", s.name, err)
+			}
+
+			err = binary.Read(reader, binary.LittleEndian, &incomingSubsystem)
+			if err != nil {
+				log.Fatalf("%s: Failed to read packet subsystem %s", s.name, err)
+			}
+
+			log.Printf("%s: Incoming subsystem %d (wanted: %d)", s.name, incomingSubsystem, s.subsystem)
+
+			log.Printf("%s: Found packet of size : %d", s.name, length)
+
+			packet := buf[pos+4 : pos+4+int(length)]
+
+			commandID := int8(packet[1])
+
+			// Check if this packet has a ZCL request id... TODO
+
+			// Check if we have any pending requests that want this command id...
+			pending := s.pendingByCommandID[commandID]
+
+			if pending != nil {
+				pending.complete <- proto.Unmarshal(packet, pending.message)
+
+			} else { // Or just send it out to be handled elsewhere
+				s.Incoming <- &packet
+			}
+
+			log.Printf("%s: Command ID:0x%X Packet: % X", s.name, commandID, packet)
+			pos += int(length) + 4
+
+			if pos >= n {
+				break
+			}
+		}
+		//fmt.Printf("Received from %s (len:%d) : % X", s.name, n, buf[:n])
+	}
 }
 
 func connectToServer(name string, subsystem int8, port int) (*ZStackServer, error) {
@@ -40,23 +209,19 @@ func connectToServer(name string, subsystem int8, port int) (*ZStackServer, erro
 		return nil, err
 	}
 
-	go func() {
-		for {
-			log.Printf("Reading from %s", name)
-			buf := make([]byte, 1024)
-			n, err := conn.Read(buf)
-			if err != nil {
-				log.Fatalf("whoops %s", err)
-			}
-			fmt.Printf("Received from %s (len:%d) : % X", name, n, buf[:n])
-		}
-	}()
+	server := &ZStackServer{
+		name:               name,
+		subsystem:          subsystem,
+		conn:               conn,
+		outgoing:           make(chan *ZStackCommand),
+		Incoming:           make(chan *[]byte),
+		pendingByCommandID: make(map[int8]*ZStackPendingResponse),
+	}
 
-	return &ZStackServer{
-		name:      name,
-		subsystem: subsystem,
-		conn:      conn,
-	}, nil
+	go server.incomingLoop()
+	go server.outgoingLoop()
+
+	return server, nil
 }
 
 func main() {
@@ -68,29 +233,67 @@ func main() {
 		log.Printf("Error connecting otasrvr %s", err)
 	}
 
-	nwkmgrConn, err := connectToServer("nwkmgr", int8(nwkmgr.ZStackNwkMgrSysIdT_RPC_SYS_PB_NWK_MGR), nwkmgrPort)
+	nwkmgrTemp, err := connectToServer("nwkmgr", int8(nwkmgr.ZStackNwkMgrSysIdT_RPC_SYS_PB_NWK_MGR), nwkmgrPort)
 	if err != nil {
 		// handle error
 		log.Printf("Error connecting nwkmgr %s", err)
 	}
-	_, err = connectToServer("gateway", int8(gateway.ZStackGwSysIdT_RPC_SYS_PB_GW), gatewayPort)
+	nwkmgrConn := &ZStackNwkMgrServer{nwkmgrTemp}
+
+	gatewayConn, err := connectToServer("gateway", int8(gateway.ZStackGwSysIdT_RPC_SYS_PB_GW), gatewayPort)
 	if err != nil {
 		// handle error
 		log.Printf("Error connecting gateway %s", err)
 	}
 
-	/*joinTime := uint32(30)
-	command := &nwkmgr.NwkSetPermitJoinReq{
-		PermitJoinTime: &joinTime,
-		PermitJoin:     nwkmgr.NwkPermitJoinTypeT_PERMIT_ALL.Enum(),
-	}*/
+	go func() {
+		for {
+			bytes := <-gatewayConn.Incoming
+
+			log.Printf("Got gateway message % X", bytes)
+
+			var message = &gateway.GwAttributeReportingInd{}
+			err = proto.Unmarshal(*bytes, message)
+			outJSON(message)
+
+		}
+	}()
 
 	//command := &nwkmgr.NwkZigbeeNwkInfoReq{}
-	command := &nwkmgr.NwkGetLocalDeviceInfoReq{}
+	response := &nwkmgr.NwkGetLocalDeviceInfoCnf{}
+	err = nwkmgrConn.SendRequest(&nwkmgr.NwkGetLocalDeviceInfoReq{}, response)
 
-	time.Sleep(2000 * time.Millisecond)
+	if err != nil {
+		log.Fatalf("Failed to get local device info: %s", err)
+	}
+	log.Println("Local device info: ")
+	outJSON(response)
 
-	nwkmgrConn.sendCommand(command, int8(command.GetCmdId()))
+	joinTime := uint32(30)
+	permitJoinRequest := &nwkmgr.NwkSetPermitJoinReq{
+		PermitJoinTime: &joinTime,
+		PermitJoin:     nwkmgr.NwkPermitJoinTypeT_PERMIT_ALL.Enum(),
+	}
+
+	permitJoinResponse := &nwkmgr.NwkZigbeeGenericCnf{}
+
+	err = nwkmgrConn.SendRequest(permitJoinRequest, permitJoinResponse)
+	if err != nil {
+		log.Fatalf("Failed to enable joining: %s", err)
+	}
+	log.Println("Permit join response: ")
+	outJSON(permitJoinResponse)
+
+	deviceListResponse := &nwkmgr.NwkGetDeviceListCnf{}
+
+	err = nwkmgrConn.SendRequest(&nwkmgr.NwkGetDeviceListReq{}, deviceListResponse)
+	if err != nil {
+		log.Fatalf("Failed to get device list: %s", err)
+	}
+	log.Println("Device list: ")
+	outJSON(deviceListResponse)
+
+	//time.Sleep(2000 * time.Millisecond)
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill)
@@ -98,31 +301,8 @@ func main() {
 
 }
 
-func (s *ZStackServer) sendCommand(command proto.Message, commandID int8) {
-	proto.SetDefaults(command)
+func outJSON(thing interface{}) {
+	jsonOut, _ := json.Marshal(thing)
 
-	packet, err := proto.Marshal(command)
-	if err != nil {
-		log.Fatal("marshaling error: ", err)
-	}
-
-	log.Printf("Protobuf packet %x", packet)
-
-	buffer := new(bytes.Buffer)
-
-	if err != nil {
-		// handle error
-		log.Printf("Error connecting %s", err)
-	}
-
-	err = binary.Write(buffer, binary.LittleEndian, uint16(len(packet)))                                // Packet length
-	err = binary.Write(buffer, binary.LittleEndian, int8(nwkmgr.ZStackNwkMgrSysIdT_RPC_SYS_PB_NWK_MGR)) // Subsystem
-	err = binary.Write(buffer, binary.LittleEndian, int8(commandID))                                    // Command Id
-	_, err = buffer.Write(packet)
-
-	log.Printf("%s: Sending packet: % X", s.name, buffer.Bytes())
-	//fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
-	//status, err := bufio.NewReader(conn).ReadString('\n')
-
-	_, err = s.conn.Write(buffer.Bytes())
+	log.Printf("%s", jsonOut)
 }
